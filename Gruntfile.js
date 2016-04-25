11941d3ca405f53226840480f8bf28843cbdcc03
Gabriel Csapo <gabecsapo@gmail.com>
2016-04-25 09:13:43 -1000

    using grunt-contrib-connect to server remote page

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,61 +9,76 @@
 'use strict';
 
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['test/screenshot'],
-    },
-
-    // Configuration to be run (and then tested).
-    screenshot: {
-      default_options: {
-        options: {
-          path: './test/screenshot',
-          remote: {
-            files: [
-              { src: "http://www.google.com", dest: "google.jpg", delay: "3000" }
-            ]
-          },
-          local: {
-            path: './test/src',
-            port: 7788,
-            files: [
-              { src: "index.html", dest: "screenshot.jpg" },
-              { src: "ajax.html", dest: "ajax.jpg", delay: "5000" }
-            ]
-          },
-          viewport: [
-            '1920x1080',
-            '1024x768',
-            '640x960'
-          ]
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
         },
-      },
-    },
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    }
-  });
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: {
+                      path: 'test/src/',
+                      options: {
+                        index: 'ajax.html',
+                        maxAge: 300000
+                      }
+                    }
+                }
+            }
+        },
+        clean: {
+            tests: ['test/screenshot'],
+        },
+        screenshot: {
+            default_options: {
+                options: {
+                    path: './test/screenshot',
+                    remote: {
+                        files: [{
+                            src: "http://localhost:8000",
+                            dest: "ajax.jpg",
+                            delay: "3000"
+                        }]
+                    },
+                    local: {
+                        path: './test/src',
+                        port: 7788,
+                        files: [{
+                            src: "index.html",
+                            dest: "screenshot.jpg"
+                        }, {
+                            src: "ajax.html",
+                            dest: "ajax.jpg",
+                            delay: "5000"
+                        }]
+                    },
+                    viewport: [
+                        '1920x1080',
+                        '1024x768',
+                        '640x960'
+                    ]
+                },
+            },
+        },
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        }
+    });
 
-  grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.registerTask('test', ['clean', 'screenshot', 'nodeunit']);
-  grunt.registerTask('default', ['jshint', 'test']);
+    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.registerTask('test', ['clean', 'connect', 'screenshot', 'nodeunit']);
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- test/screenshot_test.js
+++ test/screenshot_test.js
@@ -15,8 +15,8 @@ exports.autoshot = {
         expected = grunt.file.read('test/expected/local-1920x1080-ajax.jpg');
         test.equal(local, expected, 'should generate screenshot of sample site at local(delay)');
 
-        remote = grunt.file.read('test/screenshot/remote-1920x1080-google.jpg');
-        expected = grunt.file.read('test/expected/remote-1920x1080-google.jpg');
+        remote = grunt.file.read('test/screenshot/remote-1920x1080-ajax.jpg');
+        expected = grunt.file.read('test/expected/remote-1920x1080-ajax.jpg');
         test.equal(remote, expected, 'should generate screenshot of sample site from remote');
 
         test.done();
