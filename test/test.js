9c4af0c8126dae7560469030cb8ae36189f80014
Gabriel Csapo <gabecsapo@gmail.com>
2016-04-25 10:29:36 -1000

    updates tests to accomodate for all viewports 1920x1080, 1024x768, 640x960

--- test/test.js
+++ test/test.js
@@ -2,29 +2,82 @@ var gm = require('gm');
 var assert = require('chai').assert;
 
 describe('screenshot', function() {
-    it('should diff local image', function(done) {
-        gm.compare('test/screenshot/local-1920x1080-screenshot.jpg', 'test/expected/local-1920x1080-screenshot.jpg', function(err, isEqual, equality, raw) {
-            if (err) {
-                throw err;
-            }
-            assert.isOk(isEqual, 'images are equal');
-            assert.isAtLeast(equality, 0, 'equality is 100%');
-            console.log(equality); // eslint-disable-line no-console
-            console.log(raw); // eslint-disable-line no-console
-            done();
-        })
+
+    describe('local diffing', function() {
+        it('should diff 1920x1080', function(done) {
+            gm.compare('test/screenshot/local-1920x1080-screenshot.jpg', 'test/expected/local-1920x1080-screenshot.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            })
+        });
+        it('should diff 1024x768', function(done) {
+            gm.compare('test/screenshot/local-1024x768-screenshot.jpg', 'test/expected/local-1024x768-screenshot.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            })
+        });
+        it('should diff 640x960', function(done) {
+            gm.compare('test/screenshot/local-640x960-screenshot.jpg', 'test/expected/local-640x960-screenshot.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            })
+        });
     });
 
-    it('should diff remote images', function(done) {
-        gm.compare('test/screenshot/remote-1920x1080-ajax.jpg', 'test/expected/remote-1920x1080-ajax.jpg', function(err, isEqual, equality, raw) {
-            if (err) {
-                throw err;
-            }
-            assert.isOk(isEqual, 'images are equal');
-            assert.isAtLeast(equality, 0, 'equality is 100%');
-            console.log(equality); // eslint-disable-line no-console
-            console.log(raw); // eslint-disable-line no-console
-            done();
+    describe('remote diffing', function() {
+        it('should diff 1920x1080', function(done) {
+            gm.compare('test/screenshot/remote-1920x1080-ajax.jpg', 'test/expected/remote-1920x1080-ajax.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            });
+        });
+        it('should diff 1024x768', function(done) {
+            gm.compare('test/screenshot/remote-1024x768-ajax.jpg', 'test/expected/remote-1024x768-ajax.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            });
+        });
+        it('should diff 640x960', function(done) {
+            gm.compare('test/screenshot/remote-640x960-ajax.jpg', 'test/expected/remote-640x960-ajax.jpg', function(err, isEqual, equality, raw) {
+                if (err) {
+                    throw err;
+                }
+                assert.isOk(isEqual, 'images are equal');
+                assert.isAtLeast(equality, 0, 'equality is 100%');
+                console.log(equality); // eslint-disable-line no-console
+                console.log(raw); // eslint-disable-line no-console
+                done();
+            });
         });
     });
 });
